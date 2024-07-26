ab4df01813ce278af668c6a7da78e2157e880674
dmharrison <dmharrison2014@gmail.com>
2024-07-26 10:22:09 -0400

    added lives to user guess for a win or gameover

--- main.py
+++ main.py
@@ -3,22 +3,40 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
+print(chosen_word)
 
 display = []
-for letter in chosen_word:
-    display.append("_")
+for _ in range(len(chosen_word)):
+    display +="_"
 print(display)
+  
 
-user_guess=input("Guess a letter: ").lower()
+end_game= False
+word_length = len(chosen_word)
+lives = 6
 
 
+while not end_game:
+    user_guess = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
-    if letter == user_guess:
-        display.append(user_guess)
-    else:
-        print('incorrect')
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == user_guess:
+            display[position] = letter
+    print(display)
+    
+    if user_guess not in chosen_word:
+        lives-=1   
+    print(f"You have {lives} lives remaining ")
+    
 
-print(display )
+    if "_" not in display:
+        end_game = True
+        print("You Win")
+    elif lives == 0:
+        end_game = True
+        print("Ya Lose")
+        
+
 
 
