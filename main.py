df6daa692b73900f3cb1a66f62ac832b8fb3875a
dmharrison <dmharrison2014@gmail.com>
2024-07-26 09:35:43 -0400

    made display for user guess to the word

--- main.py
+++ main.py
@@ -3,11 +3,22 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
-print(chosen_word)
+
+display = []
+for letter in chosen_word:
+    display.append("_")
+print(display)
 
 user_guess=input("Guess a letter: ").lower()
+
+
+
 for letter in chosen_word:
     if letter == user_guess:
-        print("right")
+        display.append(user_guess)
     else:
         print('incorrect')
+
+print(display )
+
+
